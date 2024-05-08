4aae3877d57d8718cdb0ff02b6bce7af025bb0e7
Dhruvs120 <114588421+Dhruvs120@users.noreply.github.com>
2024-05-08 18:39:23 +0200

    Revert "unfuck the schedule"
    This reverts commit 3a481c068ec7cddd3376dff60952b1bb419b809a.

--- scheduleGenerator.py
+++ scheduleGenerator.py
@@ -3,10 +3,13 @@ import itertools
 def possible_schedules(dataset, requests, machines):
     days = []
     schedules = []
+    max_capacity = dataset.truck_capacity
+    
+    machine_weights = {machine.id: machine.size for machine in machines.values()}
 
     for i in range(1,dataset.days+1):
         days.append(i)
-
+    
     for r in range(1, dataset.days+1):
         for perm in itertools.permutations(days,r):
             if all(perm[i] < perm[i+1] for i in range(len(perm)-1)):
@@ -16,7 +19,9 @@ def possible_schedules(dataset, requests, machines):
                         consecutive_days = True
                         break
                 if not consecutive_days:
-                    schedules.append(list(perm))
+                    sum_weight = 0
+                    for i in perm:
+                        sum_weight += machine_weights[requests[i].machine_id] * requests[i].quantity
+                    if sum_weight <= max_capacity:
+                        schedules.append(list(perm))
     return schedules
-
-
