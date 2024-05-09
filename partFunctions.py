4fb3d6a6207f056c9ea0095ad15e775a100e5b93
Dhruvs120 <114588421+Dhruvs120@users.noreply.github.com>
2024-05-09 23:38:58 +0200

    idea

--- partFunctions.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def start_location(locations,start_location_id):
-    for location in locations.values():
-        if location.id == start_location_id:
-            return location
-    
-def distance(point1, point2):
-    return np.sqrt((point1.x - point2.x)**2 + (point1.y - point2.y)**2)
